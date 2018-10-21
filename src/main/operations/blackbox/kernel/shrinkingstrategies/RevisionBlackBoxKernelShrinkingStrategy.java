a360cf08eb9e31073c849d0f68d32294973b9a40
Luís Felipe de Melo <lipeluiis@hotmail.com>
2018-10-21 20:47:25 -0300

    Adicionando estratégia encolhe para o blackbox da revisão

--- src/main/operations/blackbox/kernel/shrinkingstrategies/RevisionBlackBoxKernelShrinkingStrategy.java
+++ src/main/operations/blackbox/kernel/shrinkingstrategies/RevisionBlackBoxKernelShrinkingStrategy.java
@@ -1,4 +1,47 @@
 package main.operations.blackbox.kernel.shrinkingstrategies;
 
-public class RevisionBlackBoxKernelShrinkingStrategy {
+import main.operations.blackbox.kernel.AbstractBlackBoxKernelShrinkingStrategy;
+import org.semanticweb.owlapi.model.OWLAxiom;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.reasoner.OWLReasoner;
+import org.semanticweb.owlapi.reasoner.OWLReasonerFactory;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class RevisionBlackBoxKernelShrinkingStrategy extends AbstractBlackBoxKernelShrinkingStrategy {
+
+    /**
+     * Creates a variation of the shrinking part of BlackBox algorithm.
+     *
+     * @param manager
+     *            the OWL ontology manager
+     * @param reasonerFactory
+     *            a factory that constructs the reasoner
+     */
+    public RevisionBlackBoxKernelShrinkingStrategy(OWLOntologyManager manager, OWLReasonerFactory reasonerFactory) {
+        super(manager, reasonerFactory);
+    }
+
+    @Override
+    public Set<OWLAxiom> shrink(Set<OWLAxiom> kb, OWLAxiom entailment, Set<OWLAxiom> inconsistent) throws OWLOntologyCreationException {
+        Set<OWLAxiom> kernelElement = new HashSet<OWLAxiom>();
+
+        OWLOntology ontology = manager.createOntology(inconsistent);
+        OWLReasoner reasoner = reasonerFactory.createNonBufferingReasoner(ontology);
+
+        for (OWLAxiom axiom : kb) {
+            if (inconsistent.contains(axiom)) {
+                manager.removeAxiom(ontology, axiom);
+                if (reasoner.isConsistent()) {
+                    kernelElement.add(axiom);
+                    manager.addAxiom(ontology, axiom);
+                }
+            }
+        }
+
+        return kernelElement;
+    }
 }
