1d10a416f4331c92558f65fec1e30f41cc751b28
Luís Felipe de Melo <lipeluiis@hotmail.com>
2018-11-18 15:03:28 -0200

    Documentando a classe de estatisticas

--- src/test/Statistics.java
+++ src/test/Statistics.java
@@ -2,26 +2,40 @@ package test;
 
 import java.util.ArrayList;
 
+/**
+ * Class with some statistics methods.
+ *
+ * @author Luís F. de M. C. Silva
+ *
+ */
 public class Statistics {
 
-    public static void getConfidenceInterval(ArrayList<Long> executionTimes) {
-        double mean = getMean(executionTimes);
-        double stdd = getStandardDeviation(executionTimes);
+    /**
+     * Prints the confidence interval of a list of values.
+     * @param values
+     */
+    public static void getConfidenceInterval(ArrayList<Long> values) {
+        double mean = getMean(values);
+        double stdd = getStandardDeviation(values);
 
-        double lower = mean - 1.96 * stdd/Math.sqrt(executionTimes.size());
-        double higher = mean + 1.96 * stdd/Math.sqrt(executionTimes.size());
+        double lower = mean - 1.96 * stdd/Math.sqrt(values.size());
+        double higher = mean + 1.96 * stdd/Math.sqrt(values.size());
 
-        // print results
         System.out.println("MÉDIA DE TEMPO = " + mean);
         System.out.print("INTERVALO DE CONFIANÇA APROXIMADO: ");
         System.out.println("[ " + lower + ", " + higher + " ]");
     }
 
-    private static double getMean(ArrayList<Long> executionTimes) {
+    /**
+     * Calculates the mean of a list of values.
+     * @param values
+     * @return the mean
+     */
+    private static double getMean(ArrayList<Long> values) {
         double sum = 0.0;
-        int size = executionTimes.size();
+        int size = values.size();
 
-        for (long time : executionTimes) {
+        for (long time : values) {
             sum += time;
         }
 
@@ -29,19 +43,30 @@ public class Statistics {
         return mean;
     }
 
-    private static double getVariance(ArrayList<Long> executionTimes) {
-        double mean = getMean(executionTimes);
+    /**
+     * Calculates the variance of a list of values.
+     * @param values
+     * @return the variation
+     */
+    private static double getVariance(ArrayList<Long> values) {
+        double mean = getMean(values);
         double sum = 0;
-        int size = executionTimes.size();
+        int size = values.size();
 
-        for(long time : executionTimes)
+        for(long time : values)
             sum += (time-mean) * (time-mean);
 
         double stdd = sum / (size - 1);
         return stdd;
     }
 
-    private static double getStandardDeviation(ArrayList<Long> executionTimes) {
-        return Math.sqrt(getVariance(executionTimes));
+    /**
+     * Calculates the standard deviation of a list of values.
+     *
+     * @param values
+     * @return the standard deviation
+     */
+    private static double getStandardDeviation(ArrayList<Long> values) {
+        return Math.sqrt(getVariance(values));
     }
 }
